766a7305a762ac55889ab4c8b5f61c16471dec35
NaMorham <namorham@gmail.com>
2020-11-23 13:27:34 +1000

    TBA-3-5-0  Updated project and solutions for VS 2013, add test files

---
+++ src/test/colours.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+echo -e "\\\\x1B[0;30m - [\x1B[0;30mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;34m - [\x1B[0;34mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;32m - [\x1B[0;32mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;36m - [\x1B[0;36mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;31m - [\x1B[0;31mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;35m - [\x1B[0;35mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;33m - [\x1B[0;33mXXX\x1B[0m]"
+echo -e "\\\\x1B[0;37m - [\x1B[0;37mXXX\x1B[0m]"
+echo
+
+echo -e "\\\\x1B[1;30m - [\x1B[1;30mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;34m - [\x1B[1;34mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;32m - [\x1B[1;32mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;36m - [\x1B[1;36mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;31m - [\x1B[1;31mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;35m - [\x1B[1;35mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;33m - [\x1B[1;33mXXX\x1B[0m]"
+echo -e "\\\\x1B[1;37m - [\x1B[1;37mXXX\x1B[0m]"
+echo
+
+echo -e "  \\\\x1B[40m - [\x1B[40mXXX\x1B[0m]"
+echo -e "  \\\\x1B[44m - [\x1B[44mXXX\x1B[0m]"
+echo -e "  \\\\x1B[42m - [\x1B[42mXXX\x1B[0m]"
+echo -e "  \\\\x1B[46m - [\x1B[46mXXX\x1B[0m]"
+echo -e "  \\\\x1B[41m - [\x1B[41mXXX\x1B[0m]"
+echo -e "  \\\\x1B[45m - [\x1B[45mXXX\x1B[0m]"
+echo -e "  \\\\x1B[43m - [\x1B[43mXXX\x1B[0m]"
+echo -e "  \\\\x1B[47m - [\x1B[47mXXX\x1B[0m]"
+echo
+
+echo -e "   \\\\x1B[5m - [\x1B[5mXXX\x1B[0m]"
+echo -e "   \\\\x1B[4m - [\x1B[4mXXX\x1B[0m]"
+echo -e "   \\\\x1B[1m - [\x1B[1mXXX\x1B[0m]"
+echo -e "   \\\\x1B[7m - [\x1B[7mXXX\x1B[0m]"
+
+echo -e "\x1B[0m\n\n"
